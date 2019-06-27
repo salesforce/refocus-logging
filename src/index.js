058def41360acf3a73dc558e55968068394c31f0
Deepanshu Gupta <guptadeepanshu@25@gmail.com>
2019-06-26 17:53:15 -0700

    add test log

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('<h1>Hello World</h1>');
 });
 
 app.listen(PORT, () => console.log('Server started'));
