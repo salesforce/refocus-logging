4d5583b7aa4c73cd56822e37fde65ba81cc8595e
Deepanshu Gupta <guptadeepanshu25@gmail.com>
2019-07-15 13:59:45 -0700

    use the right log level

--- src/handlerUtil.js
+++ src/handlerUtil.js
@@ -11,23 +11,25 @@
  * Kafka handler utilities
  */
 const logger = require('pino')();
+
+// We need to use the interal 'this' Pino uses
 const loggerTypes = {
-  error: logger.error,
-  warn: logger.warn,
-  info: logger.info,
-  debug: logger.debug,
-  trace: logger.trace,
+  error: logger.error.bind(logger),
+  warn: logger.warn.bind(logger),
+  info: logger.info.bind(logger),
+  debug: logger.debug.bind(logger),
+  trace: logger.trace.bind(logger),
 };
 
 // The default handler just logs out the message
 const defaultHandler = (messageSet, topic, partition, callback = logger.info) => {
   messageSet.forEach((m) => {
-    const key = m.message.key; // logging level
-    const value = m.message.value;
+    const key = m.message.key.toString(); // logging level
+    const value = m.message.value.toString();
 
     // TODO: Discuss different cases and what should be done
     if (loggerTypes[key]) {
-      logger.info('From application: ', topic, ' Message: ', value,
+      loggerTypes[key]('From application: ', topic, ' Message: ', value,
       ' Received at: ', new Date());
     } else {
       callback('Logging with unknown key');
